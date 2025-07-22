116b66696235732d2760b2f01415192d1da6d09f
Gabriel Rodrigues <gabriel.rodrigues.727@outlook.com>
2025-07-22 14:48:40 -0300

    commit inicial add: -arquivos e codigo base do projeto. -estilização do projeto alem de logica inicial basica

---
+++ script.js
@@ -0,0 +1,40 @@
+//constante para armazenar e manipular o formulario
+const form = document.getElementById('formAdd')
+
+
+
+//Array para para atividades e notas
+const nomes =[];
+
+
+
+let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault ();
+
+    addLinha();
+    atualizaTabela();
+});
+
+function addLinha(){
+    const inputNomeContato = document.getElementById('nomeContato');
+    const inputNumeroContato = document.getElementById('numeroContato');
+
+    if(nomes.includes(inputNomeContato.value)){
+        alert(`O contato <b>${inputNomeContato.value}</b> já existe !`)
+    }else{
+    nomes.push(inputNomeContato.value);
+    
+    let linha = `<tr><td>${inputNomeContato.value}</td><td>${inputNumeroContato.value}</td></tr>`;
+    linhas += linha;
+    }
+    inputNomeContato.value ='';
+    inputNumeroContato.value ='';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
